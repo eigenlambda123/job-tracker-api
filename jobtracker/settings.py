981ed6b64480097a5d87b4b81c107dd2fa3e34ea
Rodmark "RM" Villa <rmvilla987@gmail.com>
2025-05-11 21:25:27 +0800

    created a serializer for registering users

--- jobtracker/settings.py
+++ jobtracker/settings.py
@@ -38,8 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd party
+    'rest_framework'
+    'rest_framework.authtoken'
+
     # apps
     'users',
+
+    
 ]
 
 MIDDLEWARE = [
@@ -123,3 +129,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
