2a34ad5da04fcfd93e3265302b80c451417ed18e
Rodmark "RM" Villa <rmvilla987@gmail.com>
2025-05-13 14:21:51 +0800

    Created JobApplication Model with the following attribute:  - user  - company  - position  - status: wether pending, accepted, or rejected  - applied_dates  - notes

--- job_applications/models.py
+++ job_applications/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class JobApplication(models.model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    company = models.CharField(max_length=255)
+    position = models.CharField(max_length=255)
+    status = models.CharField(choices=[('pending', 'Pending'), ('accepted', 'Accepted'), ('rejected', 'Rejected')])
+    applied_date = models.DateField()
+    notes = models.TextField(blank=True)
