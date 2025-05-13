ed3165b80ea7e3edb64146b68a6e8f43c200bce5
Rodmark "RM" Villa <rmvilla987@gmail.com>
2025-05-13 14:42:13 +0800

    Created a JobApplicationListCreateView     - Subclasses ListCreateAPIView     - uses JobApplicationSerializer     - uses IsAuthenticated Permission
        - has get_queryset method for returning jobs only for currently authenticated user
        - has perform_create method for referencing the user = self.request.user

--- job_applications/views.py
+++ job_applications/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.permissions import IsAuthenticated
+from .models import JobApplication
+from .serializers import JobApplicationSerializer
 
-# Create your views here.
+class JobApplicationListCreateView(ListCreateAPIView):
+    serializer_class = JobApplicationSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # Return only job applications created by the currently authenticated user
+        return JobApplication.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        # Automatically associate the logged-in user with the new job application
+        serializer.save(user=self.request.user)
+
