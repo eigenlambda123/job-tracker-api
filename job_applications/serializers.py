a1237334547cb7a29874a3c353fa334fdc261868
Rodmark "RM" Villa <rmvilla987@gmail.com>
2025-05-13 14:26:23 +0800

    created serializer for JobApplication model

---
+++ job_applications/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import JobApplication
+
+class JobApplicationSerializer(serializers.ModelSerializer):
+    class Meta:
+        # uses the JobApplication model
+        model = JobApplication
+        # fields to serialize
+        fields = ['id', 'user', 'company', 'position', 'status', 'applied_date', 'notes']
