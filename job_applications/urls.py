36866ff573b32adce23eedf5c23c84ac365fbbf0
Rodmark "RM" Villa <rmvilla987@gmail.com>
2025-05-14 17:28:13 +0800

    add URLconf for JobApplicationDetailView

--- job_applications/urls.py
+++ job_applications/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import JobApplicationListCreateView
+from .views import JobApplicationListCreateView, JobApplicationDetailView
 
 urlpatterns = [
     path('applications/', JobApplicationListCreateView.as_view(), name='jobapplication-list-create'),
+    path('applications/<int:pk>/', JobApplicationDetailView.as_view(), name='job-application-detail'),
 ]
