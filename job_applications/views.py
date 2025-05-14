cab121c0c1e6dd6b8bb9f5055c204d4f5b02d024
Rodmark "RM" Villa <rmvilla987@gmail.com>
2025-05-14 17:25:17 +0800

    Add JobApplicationDetailView to handle retrieve, update, and delete operations for job applications owned by the authenticated user

--- job_applications/views.py
+++ job_applications/views.py
@@ -1,7 +1,9 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework import generics
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
 from .models import JobApplication
 from .serializers import JobApplicationSerializer
+from .permissions import IsOwnerOrReadOnly
 
 class JobApplicationListCreateView(ListCreateAPIView):
     serializer_class = JobApplicationSerializer
@@ -15,3 +17,12 @@ class JobApplicationListCreateView(ListCreateAPIView):
         # Automatically associate the logged-in user with the new job application
         serializer.save(user=self.request.user)
 
+
+class JobApplicationDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = JobApplication.objects.all()
+    serializer_class = JobApplicationSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        # Return only job applications created by the currently authenticated user
+        return JobApplication.objects.filter(user=self.request.user)
