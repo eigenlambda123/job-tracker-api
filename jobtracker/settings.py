77a1cd088ee8d9ab0c2c98bd62d7cdb6059dc366
Rodmark "RM" Villa <rmvilla987@gmail.com>
2025-05-15 11:08:26 +0800

    installed django-filter and configure it in settings

--- jobtracker/settings.py
+++ jobtracker/settings.py
@@ -137,4 +137,10 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ],
+
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ],
 }
