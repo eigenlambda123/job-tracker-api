b57a877bae0943adcbf498722caeb8964cccd546
Rodmark "RM" Villa <rmvilla987@gmail.com>
2025-05-15 13:51:21 +0800

    pagination added

--- jobtracker/settings.py
+++ jobtracker/settings.py
@@ -137,6 +137,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',  # Disable browsable API
     ],
